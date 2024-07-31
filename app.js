92ad1d4419d2ac37e8f4c7550ce11167a1e581d1
Moysed <nongalmondza12@gmial.com>
2024-07-31 14:46:21 +0700

    Change To Express node type

--- app.js
+++ app.js
@@ -7,16 +7,16 @@
 // })
 
 //AS1
-let http = require('http');
-let url = require('url');
-http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type': 'text/html'});
-/*Use the url module to turn the querystring into an object:*/
-var q = url.parse(req.url, true).query;
-/*Return the year and month from the query object:*/
-var txt = q.year + " " + q.month;
-res.end(txt);
-}).listen(9999);
+// let http = require('http');
+// let url = require('url');
+// http.createServer(function (req, res) {
+// res.writeHead(200, {'Content-Type': 'text/html'});
+// /*Use the url module to turn the querystring into an object:*/
+// var q = url.parse(req.url, true).query;
+// /*Return the year and month from the query object:*/
+// var txt = q.year + " " + q.month;
+// res.end(txt);
+// }).listen(9999);
 
 
 
@@ -36,3 +36,19 @@ res.end(txt);
 // var txt = "Name: " + q.name + " " + q.lastName + "</br>" +  "Login Date :" + q.date + " "+ q.month + " " + q.year;
 // res.end(txt);
 // }).listen(9999);
+
+
+const express = require('express')
+const app = express()
+let url = require('url');
+
+app.get('/as2', function(req, res){
+    res.set('Content-Type', 'text/html');
+    var q = url.parse(req.url, true).query;
+    var txt = "Name: " + q.name + " " + q.lastName + "</br>" +  "Login Date :" + q.date + " "+ q.month + " " + q.year;
+    res.send(txt);
+})
+
+app.listen(9999,function(){
+console.log("Server is running on PORT 9999")
+})
